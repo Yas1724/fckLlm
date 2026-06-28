a10f239209dac7fc6d88a5066bb6e27b7e7e7bae
BT06601 <Yas1724>
2026-06-28 06:21:29 +0530

    uploading inheritance.cpp file

---
+++ inheritance.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+#include<list>
+using namespace std;
+
+class youtubeChannel{
+private:
+    string Name;
+    string OwnerName;
+    int subscriberCount;
+    list<string>publishedVideoTitle;
+
+public:
+    youtubeChannel(string name, string ownername){
+        Name = name;
+        OwnerName = ownername;
+        subscriberCount = 0;
+    }
+
+    void GetInfo(){
+        cout<<"name: "<<Name<<endl;
+        cout<<"owner name: "<<OwnerName<<endl;
+        cout<<"subscribers count: "<<subscriberCount<<endl;
+        cout<<"videos: "<<endl;
+        for(string vdoTitles: publishedVideoTitle){
+            cout<<vdoTitles<<endl;
+        }
+    }
+    void subscribe(){
+        subscriberCount++;
+    }
+    void unsubscribe(){
+        if(subscriberCount>0)
+            subscriberCount--;
+    }
+    void publishedVdo(string title){
+        publishedVideoTitle.push_back(title);
+    }  
+};
+//inheriting the youtubeChannel to CodingYtChannel. 
+//CodingYtChannel = Base class & youtubeChannel = derived class. 
+class CodingYtChannel: public youtubeChannel{
+    //constructor of derived class, since the base class is private so does the derived class,
+    //we have to make the constuctor of the derived class public in order to access the derived class.
+public:    
+    CodingYtChannel(string name, string ownername):youtubeChannel(name, ownername){
+//inheriting the constructor of parent class
+    }
+//all the properties and methords of base class is inherited to the derived class
+//we can also write methords and properties only for the derived class itself, 
+        void properties(){
+            cout<<"hello";
+        }
+};
+
+int main(){
+    CodingYtChannel ytchnl("yassh", "yassh codin");
+    ytchnl.subscribe();
+    ytchnl.publishedVdo("making WAL from scratch in cpp");
+    ytchnl.GetInfo();
+    ytchnl.properties();
+}
