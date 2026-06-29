3276950717ad042893d3fc1991cea7d8066a1cb0
BT06601 <yaz1256715@gmail.com>
2026-06-29 14:13:20 +0530

    explained in much detailed way

--- virtualFunction.cpp
+++ virtualFunction.cpp
@@ -4,6 +4,7 @@ using namespace std;
 //When called through a base class pointer/reference, C++ resolves the call at runtime (dynamic dispatch) instead of compile time.
 class instrument{
 public:    
+//makeSound method maked virtual meaning, the child class might replace it or override it.
     virtual void MakeSound(){  //adding virtual keyword to make the method as virtual function
         cout<<"playing instrument..."<<endl;
     }
@@ -12,12 +13,20 @@ public:
 //and if there is no implementation of method in the derived class the execute the base class.
 class piano: public instrument{
 public:
+//piano is a child class, it has it own verison of makeSound method that override the base version
     void MakeSound(){
         cout<<"playing piano"<<endl;
     }
 };
 
 int main(){
+    //i1 is a pointer of type instrument but is actually pointing to a piano object
     instrument* i1 = new piano();
     i1->MakeSound();
 }
+
+                            /* i1 (instrument* pointer)
+                                │
+                                └──▶ piano object in memory
+                                            │
+                                            └──▶ piano::MakeSound()  ← this gets called  */
